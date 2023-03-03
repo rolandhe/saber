097a4b325fa3c2389f4aa769175b382586f38d69
hexiufeng <hexiufeng@reta-inc.com>
2023-03-03 11:59:57 +0800

    增加json rpc快捷函数

--- nfour/rpc/proto/jsonproto.go
+++ nfour/rpc/proto/jsonproto.go
@@ -37,6 +37,89 @@ type JsonProtoReq struct {
 
 type JsonProtoRes JsonProtoReq
 
+type JsonHandleBiz[T any, V any] func(tIns *T) (*V, error)
+
+func FactoryHandleBiz[T any, V any](handle JsonHandleBiz[T, V]) rpc.HandleBiz[JsonProtoReq, JsonProtoRes] {
+	return func(req *JsonProtoReq) (*JsonProtoRes, error) {
+		tIns := new(T)
+		err := json.Unmarshal(req.Body, tIns)
+		if err != nil {
+			return nil, err
+		}
+
+		v, err := handle(tIns)
+		if err != nil {
+			return nil, err
+		}
+		jv, err := json.Marshal(v)
+		if err != nil {
+			return nil, err
+		}
+		res := &JsonProtoRes{
+			Key:  req.Key,
+			Body: jv,
+		}
+		return res, nil
+	}
+}
+
+type JsonSameTypeHandleBiz[T any] func(tIns *T) (*T, error)
+
+func FactorySameTypeHandleBiz[T any](handle JsonSameTypeHandleBiz[T]) rpc.HandleBiz[JsonProtoReq, JsonProtoRes] {
+	return func(req *JsonProtoReq) (*JsonProtoRes, error) {
+		tIns := new(T)
+		err := json.Unmarshal(req.Body, tIns)
+		if err != nil {
+			return nil, err
+		}
+
+		v, err := handle(tIns)
+		if err != nil {
+			return nil, err
+		}
+		jv, err := json.Marshal(v)
+		if err != nil {
+			return nil, err
+		}
+		res := &JsonProtoRes{
+			Key:  req.Key,
+			Body: jv,
+		}
+		return res, nil
+	}
+}
+
+type JsonStringTypeHandleBiz func(s string) (string, error)
+
+func FactoryStringTypeHandleBiz(handle JsonStringTypeHandleBiz) rpc.HandleBiz[JsonProtoReq, JsonProtoRes] {
+	return func(req *JsonProtoReq) (*JsonProtoRes, error) {
+		tIns := string(req.Body)
+		v, err := handle(tIns)
+		if err != nil {
+			return nil, err
+		}
+
+		res := &JsonProtoRes{
+			Key:  req.Key,
+			Body: []byte(v),
+		}
+		return res, nil
+	}
+}
+
+func ParseJsonProtoRes[T any](res *JsonProtoRes) (*T, error) {
+	tIns := new(T)
+	if err := json.Unmarshal(res.Body, tIns); err != nil {
+		return nil, err
+	} else {
+		return tIns, nil
+	}
+}
+
+func ParseStringValueJsonProtoRes(res *JsonProtoRes) (string, error) {
+	return string(res.Body), nil
+}
+
 type jsonClientCodec[REQ JsonProtoReq, RES JsonProtoRes] struct {
 }
 
