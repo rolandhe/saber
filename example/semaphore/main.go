d900bc2a681952f2b9efe99c4058638dccee520e
hexiufeng <hexiufeng@reta-inc.com>
2023-03-20 17:03:52 +0800

    rm unstable code

--- example/semaphore/main.go
+++ example/semaphore/main.go
@@ -9,9 +9,7 @@ import (
 )
 
 func main() {
-	//waitUntil()
 	waitUntilChan()
-	//waitWithTimeout()
 	start()
 }
 
@@ -26,24 +24,6 @@ func start() {
 	<-ch
 }
 
-func waitUntil() {
-	limit := gocc.NewAtomicSemaphore(500)
-	waiter := sync.WaitGroup{}
-	waiter.Add(1000)
-	for i := 0; i < 1000; i++ {
-		go func(id int) {
-			start := time.Now().UnixNano()
-			limit.Acquire()
-			waitTime := time.Now().UnixNano() - start
-			time.Sleep(time.Millisecond * 200)
-			log.Printf("g:%d,wait:%d\n", id, waitTime)
-			limit.Release()
-			waiter.Done()
-		}(i)
-	}
-	waiter.Wait()
-}
-
 func waitUntilChan() {
 	limit := gocc.NewDefaultSemaphore(500)
 	waiter := sync.WaitGroup{}
@@ -52,30 +32,6 @@ func waitUntilChan() {
 		go func(id int) {
 			start := time.Now().UnixNano()
 			limit.Acquire()
-			waitTime := time.Now().UnixNano() - start
-			time.Sleep(time.Millisecond * 200)
-			log.Printf("g:%d,wait:%d\n", id, waitTime)
-			limit.Release()
-			waiter.Done()
-		}(i)
-	}
-	waiter.Wait()
-}
-
-func waitWithTimeout() {
-	limit := gocc.NewAtomicSemaphore(300)
-	waiter := sync.WaitGroup{}
-	waiter.Add(1000)
-	for i := 0; i < 1000; i++ {
-		go func(id int) {
-			start := time.Now().UnixNano()
-			for {
-				if limit.AcquireTimeout(time.Millisecond * 1000) {
-					break
-				}
-				log.Printf("g:%d,timeout...\n", id)
-			}
-
 			waitTime := time.Now().UnixNano() - start
 			time.Sleep(time.Millisecond * 200)
 			log.Printf("g:%d,wait:%d\n", id, waitTime)
